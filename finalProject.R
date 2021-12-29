91b6470ed6e4f1cda174ef53f50458250613feb2
Angel Wang <44830635+angel870326@users.noreply.github.com>
2021-12-29 14:12:19 +0800

    Update finalProject.R

--- finalProject.R
+++ finalProject.R
@@ -1,15 +1,17 @@
 setwd("自己打 working directory")
 
 # 只要跑一次存好檔，讀檔比較快
-# coffee <- read.csv("NTU_Coffee.csv")
-# saveRDS(coffee, file = "NTU_Coffee.rds")
+# all_merge <- read.csv("all_merge4.csv")
+# library(dplyr)
+# all_merge <- distinct(all_merge)
+# saveRDS(all_merge, file = "all_merge.rds")
 
-coffee <- readRDS("NTU_Coffee.rds")
+all_merge <- readRDS("all_merge.rds")
 
 # 不同 channel
-summary(coffee$channel)
-channel_711 <- subset(coffee, (coffee$channel=="7-11"))
-channel_louisa <- subset(coffee, (coffee$channel=="路易莎"))
-channel_family <- subset(coffee, (coffee$channel=="全家"))
-channel_starbucks <- subset(coffee, (coffee$channel=="星巴克"))
-channel_cama <- subset(coffee, (coffee$channel=="cama"))
+summary(all_merge$channel)
+channel_711 <- subset(all_merge, (all_merge$channel=="7-11"))
+channel_louisa <- subset(all_merge, (all_merge$channel=="路易莎"))
+channel_family <- subset(all_merge, (all_merge$channel=="全家"))
+channel_starbucks <- subset(all_merge, (all_merge$channel=="星巴克"))
+channel_cama <- subset(all_merge, (all_merge$channel=="cama"))
