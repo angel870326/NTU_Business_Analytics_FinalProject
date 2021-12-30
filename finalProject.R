cc7f80a85eab7d10960bc7691f4b54e44fa34bf4
jeancccc <96641483+jeancccc@users.noreply.github.com>
2021-12-30 18:26:38 +0800

    Update finalProject.R

--- finalProject.R
+++ finalProject.R
@@ -72,15 +72,16 @@ qplot(groupclock_type$clock_type, groupmonth_type$sumquant, data = groupclock_ty
       color=channel,xlab="Clock",ylab ="Quantity", 
       main ="不同時點的銷售量")+theme(plot.title=element_text(hjust = 0.5))
 
-
-# 先把size為non的拿掉
-size_nonon <-  subset(coffee, size_type!=c("non"))
-groupsize <- group_by(size_nonon, channel, size_type)%>%
+groupsize <- group_by(coffee, channel, size_type)%>%
   summarise(transactions=n(),sumtotprice=sum(totprice),
             meantotprice=mean(totprice),sumquant=sum(quant),
             meanquant=mean(quant))
-qplot(groupsize$size_type,groupsize$sumquant, data = size, color=channel,xlab="Size Type",ylab ="Quantity", 
-                 main ="不同咖啡類型的銷售總額")+theme(plot.title=element_text(hjust = 0.5))
+qplot(groupsize$size_type,groupsize$sumquant, data = groupsize, color=channel,xlab="Size Type",ylab ="Quantity", 
+      main ="不同咖啡大小的銷售量")+theme(plot.title=element_text(hjust = 0.5))
+qplot(groupsize$size_type,groupsize$sumquant, data = groupsize, color=channel,xlab="Size Type",ylab ="Total Price", 
+      main ="不同咖啡大小的銷售總額")+theme(plot.title=element_text(hjust = 0.5))
+
+
 
 
 # Model
